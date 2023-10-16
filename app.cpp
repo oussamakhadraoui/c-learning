481c0bf2a582b7b456da4aca1e342163549a5aff
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-10-16 23:57:02 +0100

    progress

--- app.cpp
+++ app.cpp
@@ -2,18 +2,12 @@
 using namespace std;
 int main()
 {
- int x ,y,z;
- x=y=z=2;
- if(x!=2){
+ int x;
+if(x==0){
 
- cout << "Hello World!\n";
- cout << "Hello World!\n" ;
- cout << "Hello World!"<< 115+23 <<"\n" ;
- cout << "Hello World!\n"<<std::endl; 
- }else{
- cout<<"there is an error";
- cout<<y+z;
- }
+ cout<<"Enter the number"<<endl;
+}
+ cout<<"error"<<endl;
  return 0;
 } 
 // search iostream
