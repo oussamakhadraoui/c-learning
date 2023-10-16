9c9fda78f705001e881a748c8791954e82deab65
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-10-16 23:44:48 +0100

    first commit

---
+++ app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int x ,y,z;
+ x=2;
+ y=3 ;
+ z=4;
+ if(x!=2){
+
+ cout << "Hello World!\n";
+ cout << "Hello World!\n" ;
+ cout << "Hello World!"<< 115+23 <<"\n" ;
+ cout << "Hello World!\n"<<std::endl; 
+ }else{
+ cout<<"there is an error";
+ cout<<y<<z;
+ }
+ return 0;
+} 
+// search iostream
