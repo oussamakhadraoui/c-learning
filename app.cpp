b8b27cfb373c03110240dbcf2836e6d037671b83
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-11-03 23:35:20 +0100

    recursion

--- app.cpp
+++ app.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <limits.h>
 using namespace std;
-string reverseString (string str )
-{
-  string x;
-  for(int i=str.length();i>=0;i--){
-    
-    x+=str[i];
-  }
-  return x;
+int dp[181] ;
+ int findCatalan(int n) {
 
-}
+    if (dp[n] != 0)
+        return dp[n];
+
+    if (n==0)
+        dp[n] = 1;
+    else
+    {
+        dp[n]=0;
+        for (int i=0 ; i<n ; i++)
+            dp[n]+=findCatalan(i)*findCatalan(n-1-i);
+    }
+    return dp[n];
+    }
 int main()
 {
-cout<<reverseString("srawedoc");
+cout<<findCatalan(175)<<endl;
 return 1;
 } 
 // search iostream
