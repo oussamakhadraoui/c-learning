44abbe32e5cb0fec3c5bd7bcd0dc96092c7bbe4a
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-11-09 22:51:51 +0100

    learning c++

--- app.cpp
+++ app.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
-#include <limits.h>
+#include <vector>
 using namespace std;
-int dp[181] ;
- int findCatalan(int n) {
 
-    if (dp[n] != 0)
-        return dp[n];
-
-    if (n==0)
-        dp[n] = 1;
-    else
-    {
-        dp[n]=0;
-        for (int i=0 ; i<n ; i++)
-            dp[n]+=findCatalan(i)*findCatalan(n-1-i);
-    }
-    return dp[n];
-    }
 int main()
 {
-cout<<findCatalan(175)<<endl;
+int n = 10;
+cout<<n<<endl;
+cout<<&n<<endl;
+
 return 1;
 } 
 // search iostream
