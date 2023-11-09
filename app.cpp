9b70f901273d043aef79b9603fe79af276a1ec0e
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-11-09 23:34:41 +0100

    c++

--- app.cpp
+++ app.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 int main()
 {
-int n = 10;
-cout<<n<<endl;
-cout<<&n<<endl;
-
-return 1;
+vector<int> v;
+v.push_back(1);
+v.push_back(10);
+v.push_back(100);
+v.push_back(1000);
+v.erase(v.begin());
+v.insert(v.begin()+6,10);
+for (vector<int>::iterator it=v.begin();it!=v.end();it++){
+    cout<<*it<<endl;
+    }
 } 
+
+
 // search iostream
