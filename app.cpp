387a8a46ef9d508993cbddf0c997282452486258
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-11-26 15:07:26 +0100

    c++

--- app.cpp
+++ app.cpp
@@ -4,17 +4,71 @@ using namespace std;
 
 int main()
 {
-vector<int> v;
-v.push_back(1);
-v.push_back(10);
-v.push_back(100);
-v.push_back(1000);
-v.erase(v.begin());
-v.insert(v.begin()+6,10);
-for (vector<int>::iterator it=v.begin();it!=v.end();it++){
-    cout<<*it<<endl;
-    }
+    int a;
+    int b;
+    int DaysNumber;
+    int final =0;
+cin>>a>>b >>DaysNumber;
+for(int i=0;i<DaysNumber;i++)
+{
+int x; 
+cin>>x;
+if(x>=a&&x<=b){
+    final++;
+}
+}
+
+cout<<final;
+    return 0;
 } 
 
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// search iostream
+
+
+bool compare(int D ,int F,int d,int f){
+bool comp=false;
+if((d>=D&&d<=F)||(f>=D&&f<=F)||(f>=F&&d<=D)){
+    comp=true;
+}
+
+
+    return comp;
+}
+int main() {
+int start,end,totalInvi;
+int final=0;
+cin>>start>>end>>totalInvi;
+for(int i =0;i<totalInvi;i++){
+    int d,f;
+    cin>>d>>f;
+   if(compare(start,end,d,f)){
+    final++;
+   }
+}
+cout<<final;
+return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int frog , tour;
+    
+cin>>frog>>tour;
+int array[frog]={0};
+for(int i = 0 ;i<tour;i++){
+   int fro,distance;
+   cin>>fro>>distance;
+   array[fro]={array[fro]+distance};
+
+}
+for(int i = 0;i<=sizeof(array);i++){
+
+ cout<<array[i];
+}
+    return 0;
+}
