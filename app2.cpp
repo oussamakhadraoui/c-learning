211c4032ff8a1abc112cdd909784eb4385b80ef8
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-08 23:14:51 +0100

    938. Range Sum of BST

--- app2.cpp
+++ app2.cpp
@@ -155,3 +155,14 @@ public:
         return result-(n*(n-1)/2);
     }
 };
+
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root == nullptr) return 0;
+        if(root->val<low)return rangeSumBST(root->right,low,high);
+         if(root->val>high)return rangeSumBST(root->left,low,high);
+        return root->val+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
+    }
+};
