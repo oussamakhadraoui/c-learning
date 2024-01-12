b99f7da4466de86ebe7a9cd7832b4df9ba7f8631
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-12 21:16:27 +0100

    leet code day 42

--- app2.cpp
+++ app2.cpp
@@ -350,3 +350,26 @@ public:
         constructGraph(root->right);
     }
 };
+class Solution {
+public:
+     int maxAncestorDiff(TreeNode* root) {
+        if (!root)
+            return 0;
+        int minVal = root->val, maxVal = root->val;
+        differ(root, minVal, maxVal);
+        return diff;
+    }
+
+private:
+    int diff = 0;
+
+    void differ(TreeNode* root, int minVal, int maxVal) {
+        if (!root)
+            return;
+        diff = max(diff, max(abs(minVal - root->val), abs(maxVal - root->val)));
+        minVal = min(minVal, root->val);
+        maxVal = max(maxVal, root->val);
+        differ(root->left, minVal, maxVal);
+        differ(root->right, minVal, maxVal);
+    }
+};
