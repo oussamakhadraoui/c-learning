d3bfa1eade85bc475ba811861ff1f9b973f5c8fa
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-13 23:58:29 +0100

    daily leetcode

--- app2.cpp
+++ app2.cpp
@@ -392,3 +392,21 @@ public:
         return x==y;
     }
 };
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int count[26] = {0};
+        // Storing the difference of frequencies of characters in t and s.
+        for (int i = 0; i < s.size(); i++) {
+            count[t[i] - 'a']++;
+            count[s[i] - 'a']--;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            ans += max(0, count[i]);
+        }
+        
+        return ans;
+    }
+};
