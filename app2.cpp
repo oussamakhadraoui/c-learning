296af68d7467d46e8e5f73e082f5cbe3fa2867ef
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-01-19 22:53:35 +0100

    daily leetcode

--- app2.cpp
+++ app2.cpp
@@ -701,3 +701,30 @@ public:
         return cleanSpaces(word,word.size()-1);
     }
 };
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n= matrix.size();
+        vector<int>row(n);
+        vector<int>prevRow(n);
+        for(int i=0;i<n;++i)row[i]=matrix[0][i];
+        for(int i=1;i<n;++i){
+            prevRow=row;
+            for(int j = 0;j<n;++j){
+              int minVal=INT_MAX;  
+                
+                minVal=min(minVal,prevRow[j]);
+                if(j-1>=0)minVal=min(minVal,prevRow[j-1]);
+                if(j+1<n)minVal=min(minVal,prevRow[j+1]);
+                
+              row[j]=matrix[i][j]+minVal;  
+            }
+        }
+        
+        int result=INT_MAX;
+        for(int i =0;i<n;++i){
+            result=min(result,row[i]);
+        }
+        return result;
+    }
+};
