216719a20fc4678dfad7cc834397c99ee7924236
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-14 23:36:43 +0100

    387. First Unique Character in a String

--- app.cpp
+++ app.cpp
@@ -1046,3 +1046,19 @@ result.push_back(pos[i]);
         }
         return result;
     }
+
+        int firstUniqChar(string s) {
+        int n=s.size();
+         vector<int> m(26,0);
+        for(char a : s){
+            m[a-'a']++;
+        }
+        int ans=-1;
+        for(int i=0;i<n;++i){
+            if(m[s[i]-'a']==1){
+                ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
