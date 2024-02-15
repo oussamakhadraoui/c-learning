647fe9e0854666916c84c1ebc3122787d2f6c08b
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-15 23:46:05 +0100

    maxSumAfterPartitioning leet code

--- app.cpp
+++ app.cpp
@@ -1150,3 +1150,18 @@ sort(nums.begin(),nums.end());
     }
         return ans;
     }
+
+        int maxSumAfterPartitioning(vector<int>& arr, int k) {
+       int n=arr.size();
+        vector<int>dp(k+1,0); 
+        for (int i =n-1;i>=0;i--){
+             int maxVal=-1, ans =0;
+            for(int j=i;j<min(n,i+k);j++){   
+               maxVal= max(maxVal,arr[j]);
+              ans =max( ans,maxVal*(j-i+1)+dp[(j+1)%k]) ;
+            } 
+             dp[i%k]=ans;
+        }
+        
+        return dp[0];   
+    }
