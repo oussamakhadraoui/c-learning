03b42e53568f5b551d009a731cd89404174d77e0
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-15 23:44:29 +0100

    sequentialDigits leet code

--- app.cpp
+++ app.cpp
@@ -1134,3 +1134,19 @@ sort(nums.begin(),nums.end());
 }
         return res;
     }
+
+       vector<int> sequentialDigits(int low, int high) {
+     string s = "123456789";
+     int x=-1;
+    vector<int> ans;
+    for(int i =2;i<=s.length();i++){
+        for(int j =0;j<=s.length()-i;j++){
+            x=stoi(s.substr(j,i)) ;
+            if(x>=low&&x<=high){
+                ans.push_back(x);
+            }
+            
+        }
+    }
+        return ans;
+    }
