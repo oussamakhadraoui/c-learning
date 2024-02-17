ea6a75d4f67ad163c0911e2adff2c5ebb6d80bea
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-17 23:44:11 +0100

    Palindrome Linked List

--- app.cpp
+++ app.cpp
@@ -1336,3 +1336,28 @@ int numSubmatrixSumTarget(std::vector<std::vector<int>>& matrix, int target) {
         }
         return result;
     }
+
+
+// Linked List
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+    bool isPalindrome(ListNode* head) {
+        vector<int>occ;
+        while(head!=NULL){
+            occ.push_back(head->val);
+            head=head->next;
+        }
+        int i=0;
+        int j=occ.size()-1;
+        while(j>i){
+            if(occ[i]!=occ[j])return false;
+            i++;j--;
+        }
+        return true;
+        
+    }
