00284be21dd4df0c491c0b582ec7f6b7c98c4743
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-19 23:50:42 +0100

    linked list

---
+++ Linked List/Middle of the Linked List.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+    ListNode* s=head;
+    ListNode* f=head;
+       
+       while(f!=NULL&&f->next!=NULL){
+
+           f=f->next->next;
+           s=s->next;
+       }
+        return s;
+
+    }
+};
