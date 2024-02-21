db319f22ee4df7e005e52d74520fb4b1f99fd01b
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-21 23:42:02 +0100

    sql training

--- sql/learning.sql
+++ sql/learning.sql
@@ -1,6 +1,6 @@
 SELECT tweet_id FROM Tweets WHERE CHAR_LENGTH(content) > 15;
 select distinct author_id as id from Views where  author_id=viewer_id order by id asc
-
+select name ,population,area from World where   area >= 3000000 OR population >= 25000000;
 
 
 -- sql training
