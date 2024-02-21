ca6750c7ac72f0d0bd11e2195715728b20821a0c
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-21 23:45:23 +0100

    sql training

--- sql/learning.sql
+++ sql/learning.sql
@@ -1,6 +1,6 @@
 SELECT tweet_id FROM Tweets WHERE CHAR_LENGTH(content) > 15;
 select distinct author_id as id from Views where  author_id=viewer_id order by id asc
 select name ,population,area from World where   area >= 3000000 OR population >= 25000000;
-
+select name from Customer where referee_id!=2 or referee_id is null ; 
 
 -- sql training
