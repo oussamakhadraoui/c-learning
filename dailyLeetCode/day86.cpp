4ea5ab256b9d3293bc3e4170a138029d932437b5
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-02-26 22:57:22 +0100

    daily leetcode

---
+++ dailyLeetCode/day86.cpp
@@ -0,0 +1,16 @@
+    struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+   
+   
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(!q&&!p)return true; 
+    if(q&&p)return q->val==p->val&& isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+    return false; 
+    }
