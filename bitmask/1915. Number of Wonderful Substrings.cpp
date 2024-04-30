c65e7ebaa7c2d5898a3da789bc18a1d02320e093
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-04-30 23:44:46 +0100

    bitmask xor

---
+++ bitmask/1915. Number of Wonderful Substrings.cpp
@@ -0,0 +1,20 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        unordered_map<int, int> count;
+        int mask = 0;
+        count[0] = 1;
+        long long ans = 0;
+        for(auto& c : word) {
+            mask ^= 1 << (c - 'a');
+            ans += count[mask];
+            for(int i = 0; i < 10; i++) {
+                ans += count[mask ^ (1 << i)];
+            }
+            count[mask]++;
+        }
+        return ans;
+    }
+};
