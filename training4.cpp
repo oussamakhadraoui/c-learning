c069ce95efeca566257dcf5e34e7718a03185e6c
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-06-12 23:45:41 +0100

    cf

--- training4.cpp
+++ training4.cpp
@@ -4,26 +4,31 @@ int main(){
  freopen("a.txt","r",stdin);
  int t;
  cin>>t;
-
  while(t--){
-  int n;
-  cin>>n;
-  int sum=0;
-  
-   int result=0;
-  for(int i=2;i<=n;i++){
-   int j=1;
-   int collector=0;
-   while(i*j<=n){
-     collector+=i*j;
-     j++;
-   }
-   if(collector>sum){
-    sum=collector;
-    result=i;
-   }
+  int x,y;
+  cin>>x>>y;
+  int len=0;
+  pair<int,int> index={-1,-1};
+  for(int i =1;i<=x;i++){
+    int counter=0;
+    pair<int,int> track ={-1,-1};
+    bool isTracked=true;
+    for(int j=1;j<=y;j++){
+        char c;
+        cin>>c;
+        if(c=='#'){
+          counter++;
+          if(isTracked){
+            track={i,j};
+          }
+        }
+    }
+    if(counter>=len){
+      len=counter;
+      index=track;
+    }
   }
-  cout<<result<<endl;
+  cout<<index.first<<" "<<index.second-(len-1)/2<<endl;
  }
  return 0;
 }
