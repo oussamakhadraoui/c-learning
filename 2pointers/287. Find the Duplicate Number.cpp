0e78b4ef7e5f327872d76501f17c0475f86d8335
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-06-17 23:57:40 +0100

    2 pointers

---
+++ 2pointers/287. Find the Duplicate Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast =nums[nums[0]];
+        while(fast!=slow){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }
+        slow=0;
+        while(fast!=slow){
+            fast=nums[fast];
+            slow=nums[slow];
+        }
+        return slow;
+    }
+};
