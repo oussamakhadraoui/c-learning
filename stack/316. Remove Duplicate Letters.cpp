f5f09af59423d4a7cb1e06c3f02ac244c63155f3
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-06-17 23:54:08 +0100

    stack

---
+++ stack/316. Remove Duplicate Letters.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int>rec;
+        int n= s.size();
+        vector<bool>seen(26,false);
+        for(int i =0;i<n;i++)rec[s[i]]=i;
+        stack<char>result;
+
+        for(int i =0;i<n;i++){
+            if(seen[s[i]-'a'])continue;
+            while(result.size()>0&&s[i]<result.top()&&rec[result.top()]>i){
+                
+                seen[result.top()-'a']=false;
+                result.pop();
+            }
+            result.push(s[i]);
+            seen[s[i]-'a']=true;
+        }
+        string ans="";
+        while(result.size()){
+            ans+=result.top();
+            result.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
