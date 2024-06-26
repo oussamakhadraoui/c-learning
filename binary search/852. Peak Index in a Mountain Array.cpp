8f8aed8a4f9bb9b8afa0a86555685cedf4b3f8dd
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-06-26 23:43:11 +0100

    binary search

---
+++ binary search/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i =0;
+        int j=arr.size()-1 ;
+        while(i<j){
+            int mid=(i+j)/2;
+            if(arr[mid]>arr[mid+1]){
+                j=mid;
+            }else{
+                i=mid+1;
+            }
+        }
+        return i;
+    }
+};
