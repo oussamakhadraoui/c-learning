a903378378c6b573e26be6b7d17ea14400ff7159
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-07-13 23:44:36 +0100

    stack training

--- training4.cpp
+++ training4.cpp
@@ -134,41 +134,102 @@
 //     }
 //     return 0;
 // }
+
+// cdbcbbaaabab
+// 012345678901
+// class Solution {
+// public:
+//     int n;
+//     int gainer(vector<char>&v,vector<int>&bad,string ss,int x){
+//         int result=0;
+//         for(int i =1;i<n;i++){
+//             if(v[i]==ss[1]&&v[i-1]==ss[0]&&!bad[i]&&!bad[i-1]){
+//                 result+=x;
+//                 bad[i]=1;
+//                 bad[i-1]=1;
+//             }
+//         }
+//         return result;
+//     }
+//     int maximumGain(string s, int x, int y) {
+//         vector<char>v;
+//         this->n=s.size();
+//         for(char &c:s)v.push_back(c);
+//         string first="ab";
+//         string second="ba";
+//         if(x<y){
+//             swap(first,second);
+//             swap(x,y);
+//         }
+//         vector<int>bad(n,0);
+//         return gainer(v,bad,first,x)+gainer(v,bad,second,y);
+//     }
+// };
+
 #include <bits/stdc++.h>
 
 using namespace std;
-// cdbcbbaaabab
-// 012345678901
 class Solution {
 public:
-    int n;
-    int gainer(vector<char>&v,vector<int>&bad,string ss,int x){
-        int result=0;
-        for(int i =1;i<n;i++){
-            if(v[i]==ss[1]&&v[i-1]==ss[0]&&!bad[i]&&!bad[i-1]){
-                result+=x;
-                bad[i]=1;
-                bad[i-1]=1;
-            }
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = healths.size();
+        vector<vector<int>>vec;
+        for(int i =0;i<n;i++){
+            vec.push_back({positions[i],healths[i],directions[i]=='R'});
+        }
+        sort(vec.begin(),vec.end(),[](vector<int>&a,vector<int>&b){
+            return a[0]<b[0];
+        });
+        stack<vector<int>>st;
+        for(int i =0;i<n;i++){
+            if(st.empty())st.push(vec[i]);
+            else if(st.top()[2]==1&&vec[i][2]==0){
+                vector<int>clone(3);
+                for(int j=0;j<3;j++)clone[j]=st.top()[j];
+                st.pop();
+                if(clone[1]>vec[i][1]){
+                    st.push({clone[0],clone[1]-1,clone[2]});
+                }else if(clone[1]<vec[i][1]){
+                    vec[i][1]--;
+                    while(!st.empty()&&vec[i][1]>st.top()[1]&&st.top()[2]){
+                        st.pop();
+                        vec[i][1]--;
+                    }
+                    
+                    if(st.empty())st.push(vec[i]);
+                    else if(!st.top()[2])st.push(vec[i]);
+                    else{
+                        vector<int>clone2(3);
+                        for(int j=0;j<3;j++)clone2[j]=st.top()[j];
+                        st.pop();
+                        // if(clone2[1]>)st.push({clone2[0],clone2[1]-1,clone2[2]});
+                        if(vec[i][1]<clone2[1]){
+                            if(clone2[1]-1>0)st.push({clone2[0],clone2[1]-1,clone2[2]});
+                        }                        
+                    }
+
+                }
+                
+            }else st.push(vec[i]);
+        }
+        unordered_map<int,int>umap;
+        vector<int>result;
+        while(!st.empty()){
+            umap[st.top()[0]]=st.top()[1];
+            st.pop();
+        }
+        for(int i =0;i<n;i++){
+            if(umap[positions[i]])result.push_back(umap[positions[i]]);
         }
         return result;
     }
-    int maximumGain(string s, int x, int y) {
-        vector<char>v;
-        this->n=s.size();
-        for(char &c:s)v.push_back(c);
-        string first="ab";
-        string second="ba";
-        if(x<y){
-            swap(first,second);
-            swap(x,y);
-        }
-        vector<int>bad(n,0);
-        return gainer(v,bad,first,x)+gainer(v,bad,second,y);
-    }
 };
 int main(){
-    
-    Solution().maximumGain("cdbcbbaaabab",4,5);
+    vector<int>p({34,50,42,2});
+    vector<int>h({6,27,17,38});
+    string s="LLRR";
+
+
+    Solution().survivedRobotsHealths(p,h,s);
     return 0;
 }
