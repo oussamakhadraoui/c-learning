06b39b0c6c389b178df32ad65659ab2c784c2196
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-07-25 23:50:28 +0100

    check bs issue

--- bs/441. Arranging Coins.cpp
+++ bs/441. Arranging Coins.cpp
@@ -2,17 +2,27 @@
 
 using namespace std;
 class Solution {
-    public int arrangeCoins(int n) {
-                int low = 1;
+public:
+    // int arrangeCoins(int n) {
+    //     int result=0;
+    //     int i=1;
+    //     while(n>=0){
+    //         result++;
+    //          n-=i;
+    //         i++;
+    //     }
+    //     return result-1;
+    // }
+    int arrangeCoins(int n) {
+        int low = 1;
         int high = n;
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
-            if (mid * (mid + 1) / 2 > n) {
-                high = (int) mid - 1;
-            } else {
-                low = (int) mid + 1;
-            }
+        while(low <= high) {
+            long long mid = low + (high - low) / 2;
+            if(mid * (mid + 1) / 2 > n) high = mid - 1;
+            else low = mid + 1;
         }
         return high;
     }
-}
+};
+
+//nlawej 3la elemnt eli arithmetic sequence sum mte3ou equal lel n;
