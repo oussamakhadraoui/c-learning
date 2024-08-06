a1cad131822147ccb9d77a5a1fb08fa4501ca043
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-06 23:51:02 +0100

    dp

--- dp/1143. Longest Common Subsequence.cpp
+++ dp/1143. Longest Common Subsequence.cpp
@@ -6,7 +6,6 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m=text1.size(),n=text2.size();
         vector<vector<int>>rec(m+1,vector<int>(n+1,0));
-            
             for(int i = m-1;i>=0;i--){
                 for(int j = n-1;j>=0;j--){
                     if(text1[i]==text2[j]){
