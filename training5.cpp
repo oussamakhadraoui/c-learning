b0e0799146cad230702f95c7015d0d16722aae89
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-07 23:39:03 +0100

    trie

--- training5.cpp
+++ training5.cpp
@@ -1,74 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> precompute_min_operations(int max_value) {
-    vector<int> min_ops(max_value + 1, 0);
-    for (int i = 1; i <= max_value; ++i) {
-        int steps = 0;
-        int num = i;
-        while (num > 0) {
-            num = num / 3;
-            steps++;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+    string word;
+    TrieNode() { 
+        memset(children, NULL, sizeof(children));
+        isEndOfWord =false;
+    }
+} ;
+class Trie {
+public:
+    TrieNode*root;
+
+    Trie(){
+        root=new TrieNode;
+    }
+    void add(string& s){
+        TrieNode*node=root;
+        for(char& c :s){
+            if(!node->children[c-'a'])node->children[c-'a']=new TrieNode;
+            node=node->children[c-'a'];
         }
-        min_ops[i] = steps;
+        node->word=s;
+        node->isEndOfWord=true;
     }
-    return min_ops;
-}
-
-int main (){
- freopen("a.txt","r",stdin);
- 
-int t;
-cin>>t;
-    const int MAX_VAL = 200000;
-    vector<int> operations = precompute_min_operations(MAX_VAL);
-while(t--){
-  int l,r;
-  cin>>l>>r;
-  vector<int>rec;
-  for(int i=l;i<=r;i++){
-     rec.push_back(i);
-  }
-  int steps=0;
-  for(int i =0;i<rec.size();i++){
-    while(rec[i+1]){
-      
+    bool search(string &s){
+        TrieNode*node=root;
+        for(char&c:s){
+            if(!node->children[c-'a'])return false;
+            node=node->children[c-'a'];
+        }
+        return node->isEndOfWord;
     }
-  }
-}
- 
-
- return 0;
-}
-
-// #include <bits/stdc++.h>
-// using namespace std;
-// vector<int>maxRc(int maxVal){
-//   vector<int>result(maxVal+1,0);
-//   for(int i=1;i<=maxVal;i++){
-//     int count=0;
-//     int num=i;
-//     while(num>0){
-//       num/=3;
-//       count++;
-//     }
-//     result[i]=count;
-//   }
-//   return result;
-// }
-// int main (){
-//  freopen("a.txt","r",stdin);
-//   int t ;
-//   cin>>t;
-//   vector<int>to=maxRc(200001);
-//   while(t--){
-//     int l,r;
-//     cin>>l>>r;
-
-//     int result=0;
-//     for(int i =l;i<=r;i++){
-//       result= max(result,to[i]);
-//     }
-//     cout<<result<<endl;
-//   }
-//  return 0;
-// }
+} ;
+int dx[]={0,0,1,-1};
+int dy[]={1,-1,0,0};
+class Solution {
+public:
+    int n ,m;
+    Trie *trie;
+    bool vis[12][12];
+    unordered_set<string>result;
+    bool isOk(int i ,int j){
+        return i>=0&&j>=0&&i<n&&j<m;
+    }
+    void dfs(int i ,int j,vector<vector<char>>& b,TrieNode*node){
+        if(node->isEndOfWord){
+            result.insert(node->word);
+        }
+        vis[i][j]=1;
+        for(int x=0;x<4;x++){
+            int row=dx[x]+i;
+            int col=dy[x]+j;
+            char c=b[row][col];
+            if(isOk(row,col)&&!vis[row][col]&&node->children[c-'a']){
+                dfs(row,col,b,node->children[c-'a']);
+            }
+        };
+        vis[i][j]=0;
+    }
+    vector<string> findWords(vector<vector<char>>& b,vector<string>& words) {
+        this->n=b.size();
+        this->m=b[0].size();
+        trie= new Trie();
+        for(string &s:words){
+            trie->add(s);
+        }
+        TrieNode *Tree=trie->root;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(Tree->children[b[i][j]-'a']){
+                    dfs(i,j,b,Tree->children[b[i][j]-'a']);
+                }
+            }
+        };
+        return vector<string>(result.begin(),result.end());
+    }
+};
