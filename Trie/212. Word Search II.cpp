15b77cc5f715d6639bbfb15d1075086bf5ae3bdb
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-08 23:55:59 +0100

    trie

---
+++ Trie/212. Word Search II.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+    string word;
+    TrieNode() { 
+        memset(children, NULL, sizeof(children));
+        isEndOfWord =false;
+        word="";
+    }
+} ;
+class Trie {
+public:
+    TrieNode*root;
+
+    Trie(){
+        root=new TrieNode;
+    }
+    void add(string& s){
+        TrieNode*node=root;
+        for(char& c :s){
+            if(!node->children[c-'a'])node->children[c-'a']=new TrieNode;
+            node=node->children[c-'a'];
+        }
+        node->word=s;
+        node->isEndOfWord=true;
+    }
+    bool search(string &s){
+        TrieNode*node=root;
+        for(char&c:s){
+            if(!node->children[c-'a'])return false;
+            node=node->children[c-'a'];
+        }
+        return node->isEndOfWord;
+    }
+} ;
+int dx[]={0,0,1,-1};
+int dy[]={1,-1,0,0};
+class Solution {
+public:
+    int n ,m;
+    Trie *trie;
+    bool vis[12][12];
+    unordered_set<string>result;
+    bool isOk(int i ,int j){
+        return i>=0&&j>=0&&i<n&&j<m;
+    }
+    void dfs(int i ,int j,vector<vector<char>>& b,TrieNode*node){
+        if(node->isEndOfWord){
+            result.insert(node->word);
+            node->isEndOfWord=false;
+        }
+        vis[i][j]=1;
+        for(int x=0;x<4;x++){
+            int row=dx[x]+i;
+            int col=dy[x]+j;
+            
+            if(isOk(row,col)&&!vis[row][col]&&node->children[b[row][col]-'a']){
+                dfs(row,col,b,node->children[b[row][col]-'a']);
+            }
+        };
+        vis[i][j]=0;
+    }
+    vector<string> findWords(vector<vector<char>>& b,vector<string>& words) {
+        this->n=b.size();
+        this->m=b[0].size();
+        trie= new Trie();
+        for(string &s:words){
+            trie->add(s);
+        }
+        TrieNode *Tree=trie->root;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(Tree->children[b[i][j]-'a']){
+                    dfs(i,j,b,Tree->children[b[i][j]-'a']);
+                }
+            }
+        };
+        return vector<string>(result.begin(),result.end());
+    }
+};
