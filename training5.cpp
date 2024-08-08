6a83d6a589df4fa0975a420daa3f85726bb0e273
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-08 23:52:32 +0100

    tree node

--- training5.cpp
+++ training5.cpp
@@ -9,6 +9,7 @@ public:
     TrieNode() { 
         memset(children, NULL, sizeof(children));
         isEndOfWord =false;
+        word="";
     }
 } ;
 class Trie {
@@ -36,47 +37,28 @@ public:
         return node->isEndOfWord;
     }
 } ;
-int dx[]={0,0,1,-1};
-int dy[]={1,-1,0,0};
 class Solution {
 public:
-    int n ,m;
-    Trie *trie;
-    bool vis[12][12];
-    unordered_set<string>result;
-    bool isOk(int i ,int j){
-        return i>=0&&j>=0&&i<n&&j<m;
-    }
-    void dfs(int i ,int j,vector<vector<char>>& b,TrieNode*node){
-        if(node->isEndOfWord){
-            result.insert(node->word);
+    Trie* trie;
+    bool dfs(int start,string&s,TrieNode*root){
+        if(start==s.size())return true;
+        bool res=false;
+        bool res2=false;
+        if(root->isEndOfWord){
+            TrieNode *clone=trie->root;
+            res=dfs(start,s,clone);
         }
-        vis[i][j]=1;
-        for(int x=0;x<4;x++){
-            int row=dx[x]+i;
-            int col=dy[x]+j;
-            char c=b[row][col];
-            if(isOk(row,col)&&!vis[row][col]&&node->children[c-'a']){
-                dfs(row,col,b,node->children[c-'a']);
-            }
-        };
-        vis[i][j]=0;
-    }
-    vector<string> findWords(vector<vector<char>>& b,vector<string>& words) {
-        this->n=b.size();
-        this->m=b[0].size();
-        trie= new Trie();
-        for(string &s:words){
+        if(root->children[s[start]-'a']) {
+            res2=dfs(start+1,s,root->children[s[start]-'a']);
+        }
+        return res||res2;
+    };
+    bool wordBreak(string s, vector<string>& wordDict) {
+        trie=new Trie;
+        for(string &s:wordDict){
             trie->add(s);
         }
-        TrieNode *Tree=trie->root;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(Tree->children[b[i][j]-'a']){
-                    dfs(i,j,b,Tree->children[b[i][j]-'a']);
-                }
-            }
-        };
-        return vector<string>(result.begin(),result.end());
+        TrieNode *root=trie->root;
+        return dfs(0,s,root);
     }
 };
