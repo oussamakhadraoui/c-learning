b6059fa5783f6b9a6d00bb34c7ace671c99e0b66
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-09 23:54:28 +0100

    trie

---
+++ training6.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h> 
+using namespace std;
+class TrieNode{
+    public:
+    TrieNode *children[26];
+    int countEndWith;
+    int countPrerixWith;
+    bool containsKey(char c){return children[c-'a']==NULL;}
+    TrieNode *get(char c){return children[c-'a'];}
+    void put(char c,TrieNode *node){children[c-'a']=node;}
+    void increaseEnd(){countEndWith++;}
+    void increasePrefix(){countPrerixWith++;}
+    void decreaseEnd(){countEndWith--;}
+    void decreasePrefix(){countPrerixWith--;}
+    int getEnd(){return countEndWith;}
+    int getEndPrefix(){return countPrerixWith;}
+};
+class Trie{
+    private:TrieNode *root;
+    public:
+
+    Trie(){
+        root=new TrieNode();
+    }
+
+    void insert(string &word){
+        TrieNode * clone=root;
+        for(char &c :word){
+            if(!clone->containsKey(c))clone->put(c,new TrieNode());
+            clone=clone->get(c);
+            clone->increasePrefix();
+        }
+        clone->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word){
+        TrieNode * clone=root;
+        for(char &c :word){
+            if(!clone->containsKey(c))return 0;;
+            clone=clone->get(c);
+        }
+        return clone->getEnd();
+    }
+
+    int countWordsStartingWith(string &word){
+        TrieNode * clone=root;
+        for(char &c :word){
+            if(!clone->containsKey(c))return 0;;
+            clone=clone->get(c);
+        }
+        return clone->getEndPrefix();        
+    }
+
+    void erase(string &word){
+        TrieNode * clone=root;
+        for(char &c :word){
+            if(!clone->containsKey(c))return;
+            clone=clone->get(c);
+            clone->decreasePrefix();
+        }
+        clone->decreaseEnd();
+    }
+};
