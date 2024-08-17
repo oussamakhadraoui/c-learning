e1ff2728c80a96f5f972cef47a02685b2c6157a3
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-17 23:49:38 +0100

    implementation

---
+++ implementation/860. Lemonade Change.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int count5=0;
+        int count10=0;
+       
+        for(int &bill:bills){
+            if(bill==5)count5++;
+            else if(bill==10){
+                if(count5){
+                    count5--;
+                    count10++;
+                }else return false;   
+            }else if(bill ==20){
+                if(count10&&count5){
+                    count10--;
+                    count5--;
+                }else if(count5>=3){
+                    count5-=3;   
+                }else return false;
+            }
+            
+        }
+        return true;
+    }
+};
