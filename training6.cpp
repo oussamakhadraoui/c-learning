1d2bbba2cb05118001d3c9c13b02b009a4e94eef
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-23 23:52:27 +0100

    odoo asse

--- training6.cpp
+++ training6.cpp
@@ -1,37 +1,42 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 
 using namespace std;
-
-const int MOD = 1e9 + 7;
-
-int countMonotonicPairs(vector<int>& nums) {
-    int n = nums.size();
-    vector<vector<int>> dp(n + 1, vector<int>(51, 0));
-    dp[0][0] = 1;
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j <= nums[i - 1]; ++j) {
-            // Transitions:
-            // 1. Current element is the same as the previous one
-            dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
-            // 2. Current element is larger than the previous one
-            if (j > 0) {
-                dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
+class Solution {
+public:
+    string fractionAddition(string s) {
+        int up =0;
+        int down=1;
+        int n =s.size();
+        int i =0;
+        int signe=1;
+        while(i<n){
+            if(s[i]=='-'){
+                signe=-1;
+                i++;
+            }else signe=1;
+            
+            int top=0;
+            while(i<n&&isdigit(s[i])){
+                top=top*10+(s[i]-'0');
+                i++;
             }
+            top*=signe;
+            i++;
+            int bot=0;
+            while(i<n&&isdigit(s[i])){
+                bot=bot*10+(s[i]-'0');
+                i++;
+            }
+            up=up*bot+top*down;
+            down=down*bot;
         }
+        int pgcd=gcd(up,down);
+        return to_string(up/pgcd)+"/"+to_string(down/pgcd);
     }
-
-    int ans = 0;
-    for (int j = 0; j <= nums[n - 1]; ++j) {
-        // For each possible last element in arr1, calculate the number of pairs
-        ans = (ans + dp[n][j]) % MOD;
-    }
-    return ans;
-}
-
+};
 int main() {
-    vector<int> nums = {2, 3, 2};
-    cout << countMonotonicPairs(nums) << endl; // Output: 4
+    string n ="1/3-1/2";
+    Solution().fractionAddition(n);
     return 0;
 }
