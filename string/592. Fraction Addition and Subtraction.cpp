85a8f0e3d8ae763e5f6e7cfc18a59bbb730024ad
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-23 23:52:14 +0100

    string

---
+++ string/592. Fraction Addition and Subtraction.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    string fractionAddition(string s) {
+        int up =0;
+        int down=1;
+        int n =s.size();
+        int i =0;
+        int signe=1;
+        while(i<n){
+            if(s[i]=='-'){
+                signe=-1;
+                i++;
+            }else{
+                signe=1;
+                if(i>0)i++;
+            }
+            int top=0;
+            while(i<n&&isdigit(s[i])){
+                top=top*10+(s[i]-'0');
+                i++;
+            }
+            top*=signe;
+            i++;
+            int bot=0;
+            while(i<n&&isdigit(s[i])){
+                bot=bot*10+(s[i]-'0');
+                i++;
+            }
+            up=up*bot+top*down;
+            down=down*bot;
+        }
+        int pgcd=gcd(up,down);
+        return to_string(up/pgcd)+"/"+to_string(down/pgcd);
+    }
+};
