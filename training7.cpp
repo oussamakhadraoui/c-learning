1d0c81327cba5cc629b7973d581b42843068fe40
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2024-08-30 23:31:42 +0100

    training

--- training7.cpp
+++ training7.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 int main (){
  freopen("a.txt", "r", stdin);
- int n;
- cin>>n;
- int result=1;
- string s;
- cin>>s;
- char signe=s[0];
- for(int i =1;i<n;i++){
-  cin>>s;
-  if(s[0]==signe)continue;
-  else{
-   result++;
-   signe=s[0];
-  }
- }
 
- cout<<result<<endl;
+ string s;
+	cin >> s;
+	int n = s.size();
+ unordered_map<string,int>rec;
+	for(int i = 1; i < n; i++){
+		if(s.substr(i, n - i) == s.substr(0, n - i) && i < n - i){
+			cout << "YES\n";
+			cout << s.substr(i, n - i);
+			return;
+		}
+	}
+	cout << "NO\n";
+
  return 0;
 }
